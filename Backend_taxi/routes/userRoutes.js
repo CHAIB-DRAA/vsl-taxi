0e1576efe5a55ddddfd79228321b878ad6ac7234
chaib-draa <chaibdraa.laid@gmail.com>
2025-08-20 23:36:32 +0200

    nouvelle config

--- Backend_taxi/routes/userRoutes.js
+++ Backend_taxi/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { signupUser, getUsers, addContact , loginUser } = require('../controllers/userController');
+const { signupUser, getUsers, addContact, loginUser } = require('../controllers/userController');
+const { authenticateUser } = require('../middleware/auth'); 
 
 // Créer un utilisateur (signup)
 router.post('/signup', signupUser);
@@ -8,11 +9,10 @@ router.post('/signup', signupUser);
 // Connexion utilisateur
 router.post('/login', loginUser);
 
-// Récupérer tous les utilisateurs
-router.get('/', getUsers);
-
-// Ajouter un contact
-router.post('/addContact', addContact);
+// Récupérer tous les utilisateurs (protégé par auth)
 router.get('/users', authenticateUser, getUsers);
+
+// Ajouter un contact (protégé par auth)
+router.post('/addContact', authenticateUser, addContact);
 
 module.exports = router;
