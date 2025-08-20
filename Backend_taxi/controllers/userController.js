b9d2c8a79daf2651bc3fbc9b31295bd1934712cb
chaib-draa <chaibdraa.laid@gmail.com>
2025-08-20 14:09:27 +0200

    nouvelle config

--- Backend_taxi/controllers/userController.js
+++ Backend_taxi/controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require('../models/User');
 
-const User = require('../models/User');
 
 // Créer ou synchroniser un utilisateur Mongo après signup Supabase
 exports.syncUser = async (req, res) => {
