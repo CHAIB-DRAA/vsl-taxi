4385672a1ca496ad8990ce8a814d37295feb16e9
chaib-draa <chaibdraa.laid@gmail.com>
2026-01-01 21:36:55 +0100

    Fix contactController remove frontend code

--- Backend_taxi/routes/userRoutes.js
+++ Backend_taxi/routes/userRoutes.js
@@ -1,8 +1,15 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { signupUser, loginUser, getUsers, addContact, searchUsers } = require('../controllers/userController');
-const authMiddleware = require('../middleware/auth'); // si tu as un middleware d’auth
+const { 
+    signupUser, 
+    loginUser, 
+    getUsers, 
+    addContact, 
+    searchUsers,
+    getProfile,    // <--- AJOUTÉ
+    updateProfile  // <--- AJOUTÉ
+  } = require('../controllers/userController');const authMiddleware = require('../middleware/auth'); // si tu as un middleware d’auth
 
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
@@ -12,7 +19,7 @@ router.post('/addContact', authMiddleware, addContact);
 router.get('/search', authMiddleware, searchUsers); // <- ici on appelle la fonction du controller
 
 
-router.get('/profile',authMiddleware, userController.getProfile);
-router.put('/profile', authMiddleware, userController.updateProfile);
+router.get('/profile', authMiddleware, getProfile);
+router.put('/profile', authMiddleware, updateProfile);
 
 module.exports = router;
