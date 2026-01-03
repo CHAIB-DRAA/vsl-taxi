681291aa4cda022b391c22ec3324308ba01c5086
chaib-draa <chaibdraa.laid@gmail.com>
2026-01-03 23:39:08 +0100

    Fix contactController remove frontend code

--- Backend_taxi/models/groups.js
+++ Backend_taxi/models/groups.js
@@ -4,7 +4,7 @@ const GroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Le créateur (Toi)
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }], // Liste des IDs des contacts
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }//tresskjgfhfg
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
