32cbe81d9c2a33e8ac6fd31fd8cbc58e22a0449a
chaib-draa <chaibdraa.laid@gmail.com>
2026-01-10 13:10:15 +0100

    Fix routes ai complete

--- Backend_taxi/models/User.js
+++ Backend_taxi/models/User.js
@@ -1,28 +1,19 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
+  fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  fullName: { type: String },
-  pushToken: { type: String },
   
-  // 👇 AJOUT SÉCURITÉ MOT DE PASSE OUBLIÉ
+  // Champs supplémentaires
+  pushToken: { type: String, default: '' },
+  contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+
+  // Champs pour le reset de mot de passe
   resetPasswordToken: String,
   resetPasswordExpires: Date
-});
 
-// Hachage automatique du mot de passe avant sauvegarde
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+}, { timestamps: true });
 
-// Méthode pour vérifier le mot de passe
-userSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
